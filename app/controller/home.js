b04149eb603501e4ae53a0edf375113dcf17048e
Eternal-could <20578466@qq.com>
2022-05-24 17:29:34 +0800

    增加/user/:id查询方式

--- app/controller/home.js
+++ app/controller/home.js
@@ -5,7 +5,13 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
-    ctx.body = 'hi, egg';
+    const { id } = ctx.query
+    ctx.body = id
+  }
+  async user() {
+    const { ctx } = this;
+    const { id } = ctx.params
+    ctx.body = id
   }
 }
 
